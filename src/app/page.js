38b078d636d220ec667b6c86360368242e3392b5
Rich Marafioti <richmarafioti@gmail.com>
2025-04-09 12:33:08 -0500

    final commit for issue

--- src/app/page.js
+++ src/app/page.js
@@ -3,22 +3,36 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <>
-      <div className={styles.page}>
-        <main>
-          <h1>Your dog called - They want a makeover.</h1>
-          <h2>Book today for the ultimate spa day.</h2>
-          <div className={styles.heroPhotoContainer}>
-            <Image
-              className={styles.heroPhoto}
-              src="https://res.cloudinary.com/do4shdwcc/image/upload/v1743860611/FullSizeRender_VSCO_5_wrmerf.jpg"
-              alt="Photo of a freshly groomed dog"
-              width={3024}
-              height={4032}
-            />
-          </div>
-        </main>
-      </div>
-    </>
+    <div className={styles.page}>
+      <main>
+        <h1>Your dog called - They want a makeover.</h1>
+        <h2 className={styles.subHeader}>
+          Book today for the ultimate spa day.
+        </h2>
+        <div className={styles.heroPhotoContainer}>
+          <Image
+            className={styles.heroPhoto}
+            src="https://res.cloudinary.com/do4shdwcc/image/upload/v1743860611/FullSizeRender_VSCO_5_wrmerf.jpg"
+            alt="Photo of a freshly groomed dog"
+            width={3024}
+            height={4032}
+          />
+        </div>
+        <section>
+          {/*<Image
+            src="https://res.cloudinary.com/do4shdwcc/image/upload/v1743860630/IMG_6897_pgvr53.heic"
+            alt="Photo of a freshly groomed dog"
+            width={3024}
+            height={4032}
+          />
+          <Image
+            src="https://res.cloudinary.com/do4shdwcc/image/upload/v1743860666/IMG_5850_v4jj2r.heic"
+            alt="Photo of a freshly groomed dog"
+            width={3024}
+            height={4032}
+          />*/}
+        </section>
+      </main>
+    </div>
   );
 }
