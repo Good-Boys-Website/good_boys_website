48bcf09ff78e9293fa5905321469572666dffece
Rich Marafioti <richmarafioti@gmail.com>
2025-06-04 20:55:50 -0500

    #72 created visibility observer hook to work dry css animation of faq and about page

--- src/app/faq/page.js
+++ src/app/faq/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+import useVisibilityObserver from "../hooks/useVisibilityObserver";
 import Image from "next/image";
 import { faqs } from "@/app/data/data";
 
@@ -8,31 +8,13 @@ import styles from "@/app/styling/faq.module.css";
 import Link from "next/link";
 
 function FaqCard({ question, answer }) {
-  const ref = useRef();
-  const [isVisible, setIsVisible] = useState(false);
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        setIsVisible(entry.isIntersecting);
-      },
-      {
-        root: null,
-        threshold: 0.6,
-      }
-    );
-
-    if (ref.current) observer.observe(ref.current);
-    return () => {
-      if (ref.current) observer.unobserve(ref.current);
-    };
-  }, []);
+  const [faqRef, faqRefVisible] = useVisibilityObserver(0.6);
 
   return (
-    <li ref={ref} className={styles.faq}>
+    <li ref={faqRef} className={styles.faq}>
       <p className={styles.question}>{question}</p>
       <div className={styles.divider}></div>
-      <p className={`${styles.answer} ${isVisible ? styles.visible : ""}`}>
+      <p className={`${styles.answer} ${faqRefVisible ? styles.visible : ""}`}>
         {answer}
       </p>
     </li>
